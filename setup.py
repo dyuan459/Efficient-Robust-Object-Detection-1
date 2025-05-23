89f58a17cc31a1360bc099934aea610024c409e5
yyuan459 <danryyuan@gmail.com>
2025-05-23 16:23:37 -0400

    testing

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open(
         "r"
 ) as f: # open annotations file to process
     annotations = json.load(f)
-    for r in annotations["categories"]: # setup names file
-        with open(
-                "C:\\Users\\Danry\\PycharmProjects\\Efficient-Robust-Object-Detection\\classes\\class.txt",
-                "a+"
-        ) as fp:
+    with open(
+            "C:\\Users\\Danry\\PycharmProjects\\Efficient-Robust-Object-Detection\\classes\\class.txt",
+            "a+"
+    ) as fp:
+        for r in annotations["categories"]: # setup names file
             if not file_check:
                 first_char = fp.read(1)
                 if not first_char: # if file is empty then start appending
@@ -20,7 +20,14 @@ with open(
                     break
             fp.write(r["name"])
             fp.write("\n")
-
+    file_check = False
     train = [a["bbox"] for a in annotations["annotations"] if a["image_id"] == 9]
-    print(train)
+    with open(
+        "C:\\Users\\yyuan459\\PycharmProjects\\Efficient-Robust-Object-Detection\\labels\\train\\000000000009.txt", "w+"
+    ) as fp:
+        if not file_check:
+            first_char = fp.read(1)
+            if not first_char: file_check = True
+            else:
+                break
 
