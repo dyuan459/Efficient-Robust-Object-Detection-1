7d4d93c43cb8c378652bad56ede0cc05e234c5fa
yyuan459 <danryyuan@gmail.com>
2025-05-29 10:29:10 -0400

    testing setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 file_check = False
 
@@ -22,9 +23,17 @@ with open(
             fp.write("\n")
     file_check = False
     bboxes = [a["bbox"] for a in annotations["annotations"] if a["image_id"] == 9] #formatted [x_top_left, y_top_left, width, height]
-    bboxes[:][0]+bboxes[:][2]/2
-
+    bboxes = np.array(bboxes)
+    bboxes[:][0]+=(bboxes[:][2]/2) # get the absolute center x coords
+    bboxes[:][1]+=(bboxes[:][3]/2) # get the absolute center y coords
+    classid = np.array([c["category_id"] for c in annotations["annotations"] if c["image_id"] == 9])
+    # right now I have absolute center x, abs center y, abs width, abs height
+    img_width = np.array([b["width"] for b in annotations["images"] if b["id"] == 9])
+    img_height = np.array([d["height"] for d in annotations["images"] if d["id"] == 9])
     print(bboxes)
+    print(classid)
+    print(img_width)
+    print(img_height)
     # with open(
     #     "C:\\Users\\yyuan459\\PycharmProjects\\Efficient-Robust-Object-Detection\\labels\\train\\000000000009.txt", "w+"
     # ) as fp:
